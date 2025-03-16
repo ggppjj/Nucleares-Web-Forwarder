e8e023141a25c443ad03921cd3f9191f7c3334f3
Gabriel Jones <1223390+ggppjj@users.noreply.github.com>
2025-03-16 10:43:31 -0400

    Cleaning up with some squash commits, part 3
    Cleaning up with some squash commits, part 2
    
    Cleaning up with some squash commits, part 1
    
    b
    
    Testing
    
    More work on linux
    
    ahhhhhhhhhhhhhh
    
    fix win32
    
    i hope this futzing doesn't end up costing me money
    
    plsssss
    
    Arm32222
    
    Arm32?
    
    keepin on keepin on
    
    how could this happen to me
    
    ahh
    
    blugh
    
    Oi
    
    pls
    
    Testing still
    
    ARM64 now.
    
    Testing
    
    Remove spaces in assemblyname.
    
    Whoops
    
    Test 2

--- Service.cs
+++ Service.cs
@@ -1,66 +1,98 @@
-﻿using Makaretu.Dns;
+﻿using System.Diagnostics;
+using Makaretu.Dns;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 
-ushort port = 8786;
+const ushort port = 8786;
+
+//const string targetProcessName = "Nucleares";
+
 var serviceDiscovery = new ServiceDiscovery();
-var serviceProfile = new ServiceProfile("Nucleares-Web-Forwarder", "_http._tcp", port);
+var domainName = new DomainName("Nucleares-Web-Forwarder");
+var serviceName = new DomainName("_http.tcp");
+var serviceProfile = new ServiceProfile(domainName, serviceName, port);
 serviceProfile.AddProperty("machine", Environment.MachineName);
-serviceDiscovery.Advertise(serviceProfile);
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Host.UseWindowsService();
-builder.Host.UseSystemd();
+builder.Host.UseSystemd(); // Ensure proper logging integration with systemd
 builder.WebHost.ConfigureKestrel(options => options.ListenAnyIP(port));
 
 var app = builder.Build();
+var httpClient = new HttpClient();
 
 app.Lifetime.ApplicationStopping.Register(() => serviceDiscovery.Unadvertise(serviceProfile));
-
-var httpClient = new HttpClient();
 
 app.Run(async context =>
 {
     var targetUri = new Uri(
-        "http://localhost:8785" + context.Request.Path + context.Request.QueryString
+        $"http://localhost:8785{context.Request.Path}{context.Request.QueryString}"
     );
-
     var forwardRequest = new HttpRequestMessage(new HttpMethod(context.Request.Method), targetUri);
 
     foreach (var header in context.Request.Headers)
     {
-        // Skip the Host header to avoid conflicts
         if (header.Key.Equals("Host", StringComparison.OrdinalIgnoreCase))
             continue;
 
         if (!forwardRequest.Headers.TryAddWithoutValidation(header.Key, [.. header.Value]))
-        {
             _ = forwardRequest.Content?.Headers.TryAddWithoutValidation(
                 header.Key,
                 [.. header.Value]
             );
-        }
     }
 
-    // Optionally, explicitly set the Host header for the target server
     forwardRequest.Headers.Host = $"{targetUri.Host}:{targetUri.Port}";
 
     if (context.Request.ContentLength > 0)
         forwardRequest.Content = new StreamContent(context.Request.Body);
 
     using var responseMessage = await httpClient.SendAsync(forwardRequest);
-
     context.Response.StatusCode = (int)responseMessage.StatusCode;
+
     foreach (var header in responseMessage.Headers)
         context.Response.Headers[header.Key] = header.Value.ToArray();
 
     foreach (var header in responseMessage.Content.Headers)
         context.Response.Headers[header.Key] = header.Value.ToArray();
 
-    // Remove transfer-encoding header if present
     _ = context.Response.Headers.Remove("transfer-encoding");
 
     await responseMessage.Content.CopyToAsync(context.Response.Body);
 });
 
 app.Run();
+
+
+//TODO: Fix linux process monitoring.
+//await MonitorProcessAndRunServer();
+//return;
+
+//bool IsProcessRunning(string name)
+//{
+//    return Process.GetProcessesByName(name).Length > 0 ||
+//           Process.GetProcessesByName(name.ToLower()).Length > 0;
+//}
+
+//void Log(string message)
+//=> Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {message}");
+
+
+//async Task MonitorProcessAndRunServer()
+//{
+//    Console.WriteLine("Starting process monitor...");
+//    Console.Out.Flush();
+//    while (true)
+//    {
+//        if (IsProcessRunning(targetProcessName))
+//        {
+//            Log($"{targetProcessName} detected. Starting forwarder.");
+//            serviceDiscovery.Advertise(serviceProfile);
+//            await app.RunAsync();
+//            serviceDiscovery.Unadvertise(serviceProfile);
+//            Log($"{targetProcessName} stopped. Forwarder shutting down.");
+//        }
+//
+//        await Task.Delay(5000); // Check every 5 seconds
+//    }
+//    // ReSharper disable once FunctionNeverReturns
+//}
